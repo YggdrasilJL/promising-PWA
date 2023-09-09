4b4b9ac36418a67d8d290152ed38b52553649ec4
YggdrasilJL <yggdrasiljl@gmail.com>
2023-09-09 17:46:09 -0300

    make changes to database.js, further debugging

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,27 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  console.log('PUT to the database');
+export const getDb = async (val) => {
+  console.log('Getting data from the jateDB');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, text: content });
-  const result = await request;
-  console.log('Data saved to the database', result.text);
-};
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  console.log('GET all from the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('data saved to the jateDB', result);
+};
+
+export const putDb = async (id, val) => {
+  console.log('PUT request to update the jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, value: val });
+  const result = await request;
+  console.log('data saved to the jateDB', result);
 };
 
 initdb();
